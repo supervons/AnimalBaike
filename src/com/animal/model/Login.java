5ea7c8b48a17906e796c3ef617367ef630fbfa51
fys <superfys@sina.com>
2018-04-22 17:43:27 +0800

    新增实体类以及数据库修改

--- src/com/animal/model/Login.java
+++ src/com/animal/model/Login.java
@@ -1,5 +1,7 @@
 package com.animal.model;
-
+/**
+ * 用户登陆表
+ */
 import java.util.Date;
 
 import org.springframework.stereotype.Component;
@@ -9,6 +11,9 @@ public class Login {
 	private String userPassword;
 	private int userIdentity;
 	private Date userRegitime;
+	private String reserveField1;
+	private String reserveField2;
+	private String reserveField3;
 	
 	public String getUserId() {
 		return userId;
@@ -42,12 +47,39 @@ public class Login {
 		this.userRegitime = userRegitime;
 	}
 
+	public String getReserveField1() {
+		return reserveField1;
+	}
+
+	public void setReserveField1(String reserveField1) {
+		this.reserveField1 = reserveField1;
+	}
+
+	public String getReserveField2() {
+		return reserveField2;
+	}
+
+	public void setReserveField2(String reserveField2) {
+		this.reserveField2 = reserveField2;
+	}
+
+	public String getReserveField3() {
+		return reserveField3;
+	}
+
+	public void setReserveField3(String reserveField3) {
+		this.reserveField3 = reserveField3;
+	}
+
 	@Override
 	public String toString() {
 		return "Login [userId=" + userId + ", userPassword=" + userPassword
 				+ ", userIdentity=" + userIdentity + ", userRegitime="
-				+ userRegitime + "]";
+				+ userRegitime + ", reserveField1=" + reserveField1
+				+ ", reserveField2=" + reserveField2 + ", reserveField3="
+				+ reserveField3 + "]";
 	}
+
 	
 	
 }
